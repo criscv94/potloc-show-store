bf2cb9b6294196ed59c551d75f7705ebabb90466
Cristian Cortes <cacortes1@uc.cl>
2023-02-05 15:42:12 -0300

    ⚡️ add missing includes store on query

--- potloc-api/app/controllers/store_items_controller.rb
+++ potloc-api/app/controllers/store_items_controller.rb
@@ -1,6 +1,6 @@
 class StoreItemsController < ApplicationController
   def index
-    @store_items = StoreItem.joins(:item).all
+    @store_items = StoreItem.joins(:item, :store).all
     render json: StoreItemSerializer.new(@store_items), status: 200
   end
 end
